692318feea57ddd4d13179130127a2b92a2975df
Jaydip Rathod <108605829+jaydipr4646@users.noreply.github.com>
2023-06-30 18:02:45 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 nltk.download('stopwords')
-nltk.download('punkt')`
+nltk.download('punkt')
 from nltk.stem import PorterStemmer
 
 port_stemmer = PorterStemmer()
